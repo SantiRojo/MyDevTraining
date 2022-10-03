f2f32350ea8c3c0553a02bd8d0830a03e2fceeb7
Santi Rojo <santi.rojo.franco@gmail.com>
2022-10-03 13:06:06 -0300

    Hasta Métodos de Arrays

--- myScript.js
+++ myScript.js
@@ -1,4 +1,4 @@
-let nombre = "Santi";
+/* let nombre = "Santi";
 let apellido = "Rojo";
 let age;
 let phoneNumber = "+54 9 (0351) 152262561";
@@ -27,7 +27,7 @@ if(age >= 18){
 
 for(let i = 0; i <= 20; i++){
     console.log(i);
-};
+}; */
 
 /* let num = parseInt(prompt("Ingrese el número del que quiere saber la tabla del 1 al 10"))
 
@@ -121,3 +121,141 @@ alert("El nuevo precio es $"+nuevoPrecio); */
 
 // OBJETOS
 
+//Ejemplo agenda:
+
+/* let nombre = "Santi";
+let edad = 27;
+let calle = "Jerónimo Luis de Cabrera 964" */
+
+/* const persona1 = {
+    nombre: "Santi",
+    edad: 27,
+    calle: "Jerónimo Luis de Cabrera 964"
+}
+
+console.log(persona1); */
+
+//Métodos de acceso a las propiedads de un objeto
+
+//Con punto (.)
+/* console.log(persona1.nombre);
+console.log(persona1.edad);
+console.log(persona1.calle); */
+
+//Con corchetes []
+
+/* console.log(persona1["nombre"]);
+console.log(persona1["edad"]);
+console.log(persona1["calle"]); */
+
+// Asignando valores
+
+// Con punto
+/* persona1.edad = 26; */
+
+//Con corchetes
+
+/* persona1["calle"] = "Rafael de Igarzabal 760";
+
+console.log(persona1) */
+
+// Constructores
+
+/* function Persona(nombre, edad, calle){
+    this.name = nombre;
+    this.age = edad;
+    this.address = calle;
+} */
+
+// Instancia
+
+/* const persona1 = new Persona("Santi",27,"Jerónimo Luis de Cabrera 964");
+
+const persona2 = new Persona("Fer",55,"Rafael de Igarzabal 760");
+
+console.log(persona1);
+console.log(persona2); */
+
+// Definición de métodos personalizados
+
+/* function Persona(nombre, edad, calle){
+    this.name = nombre;
+    this.age = edad;
+    this.address = calle;
+    this.saludar = function(){console.log("Hola, soy "+this.name)}
+}
+
+const persona1 = new Persona("Santi",27,"Jerónimo Luis de Cabrera 964");
+
+const persona2 = new Persona("Fer",55,"Rafael de Igarzabal 760");
+
+persona1.saludar();
+persona2.saludar(); */
+
+// Clases
+
+/* class Persona {
+    constructor(nombre,edad,calle){
+        this.name = nombre,
+        this.age = edad,
+        this.address = calle;
+    };
+
+    saludar(){
+        alert("Hola! Soy "+this.name);
+    };
+
+    decirEdad(){
+        alert("Tengo "+this.age+" años.");
+    };
+};
+
+const persona1 = new Persona("Santi",27,"Jerónimo Luis de Cabrera 964");
+const persona2 = new Persona("Fer",54,"Rafael de Igarzabal 760");
+
+persona1.saludar();
+persona1.decirEdad();
+
+persona2.saludar();
+persona2.decirEdad(); */
+
+// ARRAYS
+
+//Declaración de array vacío
+const arrayA = [];
+
+//Declaración de array con números
+const arrayB = [15,10,1995];
+
+//Declaración de array con strings
+const arrayC = ["Santiago", "Fermín", "Rojo"];
+
+//Declaración de array con booleanos
+const arrayD = [true,false,false,true];
+
+//Declaración de array mixto
+const arrayE = [15,"Rojo",true];
+
+//Ejemplo de operaciones con arrays homogéneos
+
+const numeros = [1,2,3,4,5,6,7,8,9,10];
+
+let resultado1 = numeros[3] + numeros[7];
+let resultado2 = numeros[5] - numeros[2];
+let resultado3 = numeros[9] / numeros[1];
+
+console.log(resultado1);
+console.log(resultado2);
+console.log(resultado3);
+
+//Recorriendo el array con ciclo For
+
+for (let i = 0; i < 10; i++){
+    alert(numeros[i]);
+}
+
+//Recorriendo el array con ciclo For sin saber la longitud del array
+
+for(let i = 0; i < numeros.length;i++){
+    console.log(numeros[i]);
+}
